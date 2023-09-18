add9d605c45a68089e52f46a21ef4d2a3037ab2f
Bruno <bframos9@gmail.com>
2023-09-18 20:06:52 +0100

    import bacano

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id=models.CharField(max_length=250,primary_key=True)
+    title=models.CharField(max_length=250, default="N/A")
+    subtitle=models.CharField(max_length=250, default="N/A")
+    authors=models.CharField(max_length=250, default="N/A")
+    publisher=models.CharField(max_length=250, default="N/A")
+    published_date=models.CharField(max_length=250,default="N/A")
+    category=models.CharField(max_length=250, default="N/A")
+    distribution_expense=models.FloatField(default=0)
+    class Meta:
+        db_table='Book'
+
