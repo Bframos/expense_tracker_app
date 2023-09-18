41050e20a90eac8e512663e250cfd95aa0672649
Bruno <bframos9@gmail.com>
2023-09-18 18:27:45 +0100

    commit

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    
+    title=models.CharField(("title"),max_length=255)
+    subtitle=models.CharField(("subtitle"),max_length=255)
+    authors=models.CharField(("authors"),max_length=255)
+    published_date=models.DateField(("published_date"),auto_now=True)
+    category=models.CharField(("category"),max_length=255)
+    distribution_expense=models.FloatField(("distribution_expense"),default=0)
+
+
+
+
+
+
